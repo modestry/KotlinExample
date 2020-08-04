35651dab7bec7fb8300418b7fd29e6d6e19ed565
modestry <modestry2007@yandex.ru>
2020-08-04 23:21:53 +0300

    1

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,14 +1,1 @@
 package ru.skillbranch.kotlinexample.extensions
-
-fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
-    var list = this.toMutableList()
-    var i = this.size
-    while(--i >= 0)    {
-        if(predicate(list[i])) {
-            list.remove(list[i])
-            break
-        }
-        list.remove(list[i])
-    }
-    return list
-}
